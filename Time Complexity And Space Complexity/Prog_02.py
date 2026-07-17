517c112a058d788cd55b58281225bfa0f556d033
Musawer Hussain <147225635+Musawerhussainkhoso@users.noreply.github.com>
2026-07-17 12:08:22 +0500

    feat: add script to calculate sales total and document time and space complexity

---
+++ Time Complexity And Space Complexity/Prog_02.py
@@ -0,0 +1,9 @@
+sales = [1200, 1500, 900, 2000, 1750]
+sum =0 
+for i in sales:
+    sum +=i
+print("Total sales:", sum)
+#time complexity
+print("Time Complexity : o(n)")
+print("Space Complexity : o(1)")
+
