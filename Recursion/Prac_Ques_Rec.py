6b7940a5f36ec1966d03a51ae00a29cdcc5aae74
Musawer Hussain <147225635+Musawerhussainkhoso@users.noreply.github.com>
2026-07-18 19:24:43 +0500

    feat: add recursive implementations for printing numbers from 1 to N and N to 1

--- Recursion/Prac_Ques_Rec.py
+++ Recursion/Prac_Ques_Rec.py
@@ -1,11 +1,19 @@
 #Given a number N, print all numbers from 1 to N using recursion.
 inp = int(input("Enter a number : "))
-num = 1
 def print_num(num):
     if num > inp:
         return 
     print(num)
     print_num(num+1)
-print_num(num)    
+print_num(1)    
+
+#reverse 
+inp = int(input("Enter a number : "))
+def print_num(num):
+    if num < 1:
+        return 
+    print(num)
+    print_num(num - 1)
+print_num(inp)
 
 
